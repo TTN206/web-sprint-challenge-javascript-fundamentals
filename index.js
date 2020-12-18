e93641fdbfad0d60cc6fe5d65c801844999c32ed
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2020-12-18 08:12:57 -0800

    firstBlood

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+//  because they're able to reach inside and invoke the func and loop that around
 
 
 
@@ -28,10 +28,12 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+  for(let num =0; num < 10, num++){
+    let count = 0;
   }
+  return count ++;
+}
  
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
@@ -56,8 +58,10 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function animalNames(displayNames){
+    zooAnimals.forEach(displayNames(indX)){
+      return "name: {name}, scientific: {scientific name}";
+    }
   }
   
 
